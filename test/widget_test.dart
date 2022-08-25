f286c31784a85fa066dc60b18858c108482b8e4d
Craig Maruziva <craig.maruziva@withwise.com>
2022-08-25 10:02:20 +0100

    workflow changed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:music_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Notes test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MusicApp());
 
